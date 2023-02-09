d9e7666f09c415e89de605cc1f8ac12bd2360539
msjopyho <117312312+msjopyho@users.noreply.github.com>
2023-02-09 11:56:11 -0600

    base formats of tutorial

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    request_method = request.method
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        return redirect(url_for('name', first_name=first_name))
+    return render_template('hello.html', request_method=request_method)
+
+
+@app.route('/name/<string:first_name>')
+def name(first_name):
+    return f'{first_name}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
