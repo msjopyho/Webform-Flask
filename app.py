2c723fe1ead95ccdcb5b4a5a0927264da634f452
msjopyho <117312312+msjopyho@users.noreply.github.com>
2023-02-09 12:39:38 -0600

    updated a /forms page that collects data

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
+from forms import Todo
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'password' #protect against cross site request forgery
+app.config['SECRET_KEY'] = 'password'  # protect against cross site request forgery
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -15,6 +17,15 @@ def hello_world():
 @app.route('/name/<string:first_name>')
 def name(first_name):
     return f'{first_name}'
+
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
+    todo_form = Todo()
+    if todo_form.validate_on_submit():
+        print(todo_form.content.data)
+        redirect('/')
+    return render_template('todo.html', form=todo_form)
 
 
 if __name__ == '__main__':
