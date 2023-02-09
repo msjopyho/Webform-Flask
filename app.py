44d64483baa00603170fc7588e49418f04b220a7
msjopyho <117312312+msjopyho@users.noreply.github.com>
2023-02-09 12:04:02 -0600

    create secret key (protect against csrf)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'password' #protect against cross site request forgery
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
